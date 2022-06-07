be4f8f50d0a2cd9c3112846a84cf1003c5861f06
RajuJangid <95289422+RajuJangid@users.noreply.github.com>
2022-06-07 22:14:27 +0530

    updated js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+let raju = document.querySelector(".hresp");
+let jangid = document.querySelector(".vresp");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
+    raju.classList.toggle("active");
+    jangid.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    raju.classList.remove("active");
+    jangid.classList.remove("active");
 }))
